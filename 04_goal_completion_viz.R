955f162eea712baac7ec3ac88d83d81b01479d46
Edimah <98950952+Edimah@users.noreply.github.com>
2025-11-13 17:33:24 +0100

    completion viz

--- 04_goal_completion_viz.R
+++ 04_goal_completion_viz.R
@@ -9,3 +9,19 @@ library(lubridate)
 # LOAD : ACTIVITY SUMMARY DATA
 # ------------------------------
 activity_summary <- vroom("apple-health-data/data/health_data_activitysummary.csv", col_types = cols())
+
+
+# ------------------------------
+# COMPUTE GOAL COMPLETION DELTAS
+# ------------------------------
+
+activity_summary <- activity_summary %>%
+    mutate(
+        activeEnergyBurnedGoalDelta = activeEnergyBurned - activeEnergyBurnedGoal,
+        appleExerciseTimeGoalDelta = appleExerciseTime - appleExerciseTimeGoal,
+        standHoursGoalDelta = appleStandHours - appleStandHoursGoal
+    )
+
+# ------------------------------
+# PLOT: Goal Completion Time Series
+# ------------------------------
